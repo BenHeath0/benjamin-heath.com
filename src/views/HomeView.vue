53b58cf0cfb20493d7eafb9f71ff310edc8bb16b
Ben Heath <ben.heath@rechargeapps.com>
2024-12-30 15:52:02 -0500

    chore: cleanup copy on home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,12 @@
   <div class="px-10 py-5 text-lg text-center">
     <p class="py-2">
       Hi, my name is Ben Heath. I'm a software engineer based out of Boston working
-      as a fullstack engineer at Recharge. Previously I worked at MITRE building software
+      as a fullstack engineer at Recharge, an ecommerce startup. Previously I worked at MITRE building software
       in the public interest
     </p>
 
     <p class="py-2">
-      I got bored one day and decided to build this very boring basic website. Maybe i'll
-      do something fun with it one day
+      There isn't a whole lot here yet, but I'm working on it.
     </p>
   </div>
 </template>
