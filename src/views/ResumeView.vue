5beb1a2e0bf26187a15d8abe9719ffed2ea02fad
Ben Heath <ben.heath@rechargeapps.com>
2024-12-30 11:25:11 -0500

    feat: add resume page

---
+++ src/views/ResumeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="px-10">
+      <p class="py-5">
+        Heres my latest working resume. Don't mind the TODOs
+      </p>
+      <PDF src="/resume.pdf" />
+    </div>
+  </template>
+  
+  <script setup>
+  import PDF from "pdf-vue3";
+  </script>
